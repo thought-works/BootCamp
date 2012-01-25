9ad95e90dfd08f1f21a9e3980cc2f26e86083b36
Ryan Puskas <rpuskaspublic@gmail.com>
2012-01-25 10:18:05 -0600

    who has no live 2

--- zoltar.rb
+++ zoltar.rb
@@ -9,6 +9,10 @@ class Zoltar
   def self.attempting_to_create_merge_conflict
     puts 'me agian'
   end
+  
+  def self.i_have_no_life
+    puts 'yup this is ryan p'
+  end
 end
 
 Zoltar.what_is_my_favorite_movie
